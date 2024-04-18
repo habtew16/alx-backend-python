994e61b413637cb6fc9533ec99beda554e75e797
habtew16 <habtamuwolde96@gmail.com>
2024-04-18 20:42:13 +0000

    chanhged annot

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@ list of numbers and floats
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union(float, int)) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
     '''
     Computes the sum of a list of integers and floating numbers.
     '''
