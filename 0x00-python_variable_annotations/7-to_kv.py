ef60c5574d1d3685f58ab43686a3599e898ae6f9
habtew16 <habtamuwolde96@gmail.com>
2024-04-18 20:44:02 +0000

    removed { and replaced )

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -9,4 +9,4 @@ def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
     '''
     Computes the sum of a list of integers and floating numbers.
     '''
-    return (k, float(v**2})
+    return (k, float(v**2))
