0e6e59080d8a339d1afbfc0b91e41266593a8d2e
habtew16 <habtamuwolde96@gmail.com>
2024-04-18 20:03:19 +0000

    concat string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+string type annotated
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    function to concat and return
+    two strings
+    '''
+    return str1 + str2
